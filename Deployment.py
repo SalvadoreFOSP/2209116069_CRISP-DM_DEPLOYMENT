a824204e9e58fb7ade1cb58767b63f3bb29f37c3
SalvadoreFOSP <126777568+SalvadoreFOSP@users.noreply.github.com>
2024-04-23 11:22:38 +0800

    Update Deployment.py

--- Deployment.py
+++ Deployment.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import seaborn as sns
+import matplotlib as plt
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import MinMaxScaler
 
